31fa47c777ae2baff3b9ff8a07326886d39b2792
Fabio <fabio.calcagno@fatec.sp.gov.br>
2020-01-22 12:36:37 -0300

    header-resposnivo ok

--- src/app/cards/styles/cards-style2.directive.ts
+++ src/app/cards/styles/cards-style2.directive.ts
@@ -1,4 +1,4 @@
-import { Directive, ElementRef,Renderer, OnInit, Input, HostListener } from '@angular/core';
+import { Directive, ElementRef,Renderer2, OnInit, Input, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appCardsStyle2]'
@@ -7,20 +7,34 @@ export class CardsStyle2Directive implements OnInit{
  
 
   constructor(private el: ElementRef,
-              private renderer: Renderer) { }
+              private renderer: Renderer2) { }
 
 
-              @Input() styleColor:string;
+              @Input() clicked:boolean;
               
-              
+              @HostListener('click')
+              toggleMenuBar(){
+                if(this.clicked){
+                  this.el.nativeElement.querySelector('.links-navigation').style.display = "none";
+                  this.el.nativeElement.querySelector('.enter-or-buy-nav').style.display = "none";
+                  this.clicked = false;
+                }
+                else {
+                  this.renderer.setStyle(this.el.nativeElement, "paddingTop", "5%")     
+                  this.el.nativeElement.querySelector('.links-navigation').style.display = "flex";
+                  this.el.nativeElement.querySelector('.enter-or-buy-nav').style.display = "flex";
+                  
+                  this.clicked = true
+              } 
+            }
+
+          
+
 
   ngOnInit(): void {
       
-    
-    this.renderer.setElementAttribute(this.el.nativeElement, 'margin-bottom-style'); 
+
     
   }
-
-  
 
 }
