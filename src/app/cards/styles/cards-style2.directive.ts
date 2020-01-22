5daea34025f4e5147cc787c6707b9faa336956b6
Fabio <fabio.calcagno@fatec.sp.gov.br>
2020-01-22 15:19:19 -0300

    header responsive fixed

--- src/app/cards/styles/cards-style2.directive.ts
+++ src/app/cards/styles/cards-style2.directive.ts
@@ -1,39 +1,66 @@
-import { Directive, ElementRef,Renderer2, OnInit, Input, HostListener } from '@angular/core';
+import { Directive, ElementRef,Renderer2, OnInit, Input, HostListener, OnChanges, DoCheck } from '@angular/core';
+
 
 @Directive({
   selector: '[appCardsStyle2]'
 })
-export class CardsStyle2Directive implements OnInit{
- 
+export class CardsStyle2Directive{
+  
 
   constructor(private el: ElementRef,
               private renderer: Renderer2) { }
 
 
-              @Input() clicked:boolean;
+              @Input() largura:any;
+
+              private clicked:boolean;
               
               @HostListener('click')
               toggleMenuBar(){
-                if(this.clicked){
-                  this.el.nativeElement.querySelector('.links-navigation').style.display = "none";
-                  this.el.nativeElement.querySelector('.enter-or-buy-nav').style.display = "none";
-                  this.clicked = false;
-                }
-                else {
-                  this.renderer.setStyle(this.el.nativeElement, "paddingTop", "5%")     
-                  this.el.nativeElement.querySelector('.links-navigation').style.display = "flex";
-                  this.el.nativeElement.querySelector('.enter-or-buy-nav').style.display = "flex";
-                  
-                  this.clicked = true
-              } 
+                    this.largura = window.innerWidth
+                    console.log(this.largura)
+                    
+               if(this.largura <= 1000){
+
+                    if(this.clicked){
+                      this.el.nativeElement.querySelector('.links-navigation').style.display = "none";
+                      this.el.nativeElement.querySelector('.enter-or-buy-nav').style.display = "none";
+                      this.el.nativeElement.querySelector('#menu-responsivo').style.transform = "rotate(0deg)"
+                      this.clicked = false;
+                    }
+                    else {
+                      this.renderer.setStyle(this.el.nativeElement, "paddingTop", "5%")
+                      this.el.nativeElement.querySelector('.links-navigation').style.display = "flex";
+                      this.el.nativeElement.querySelector('.enter-or-buy-nav').style.display = "flex";
+                      this.el.nativeElement.querySelector('#menu-responsivo').style.transform = "rotate(180deg)"
+                      
+                      this.clicked = true
+                  } 
+            
+               }
+               else{
+                this.el.nativeElement.querySelector('.links-navigation').style.display = "flex";
+                this.el.nativeElement.querySelector('.enter-or-buy-nav').style.display = "flex";
+               } 
+
+               
+             
             }
 
-          
+            @HostListener('window:resize', ['$event'])
+            onResize(event) {
+             let a =  event.target.innerWidth;
+             if(a > 1000){
+              this.el.nativeElement.querySelector('.links-navigation').style.display = "flex";
+              this.el.nativeElement.querySelector('.enter-or-buy-nav').style.display = "flex";
+             }
+            }
+
+        
 
 
   ngOnInit(): void {
-      
-
+   
     
   }
 
